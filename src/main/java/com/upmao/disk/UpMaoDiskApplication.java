ffa5071ec7b9ab17848e11ff8063f2217bc62127
Cat-Mng <supermao2022@163.com>
2023-08-30 17:25:15 +0800

    Initial commit

---
+++ src/main/java/com/upmao/disk/UpMaoDiskApplication.java
@@ -0,0 +1,13 @@
+package com.upmao.disk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpMaoDiskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpMaoDiskApplication.class, args);
+    }
+
+}
